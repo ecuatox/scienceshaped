548d3d312f10fa67b6e11888c5ea12c4b3841442
ecuatox <a.tenstad@gmail.com>
2016-03-18 21:44:38 +0100

    Rename project to projects

--- projects/apps.py
+++ projects/apps.py
@@ -3,5 +3,5 @@ from __future__ import unicode_literals
 from django.apps import AppConfig
 
 
-class ProjectConfig(AppConfig):
-    name = 'project'
+class ProjectsConfig(AppConfig):
+    name = 'projects'
--- scienceshaped/urls.py
+++ scienceshaped/urls.py
@@ -1,10 +1,11 @@
 from django.conf import settings
 from django.conf.urls.static import static
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
 from . import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.index, name='index'),
+    url(r'^projects/', include('projects.urls')),
 ]
