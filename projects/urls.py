3e220f5f15344775dedb775986ba8ec9602abbc8
ecuatox <a.tenstad@gmail.com>
2016-03-20 22:05:30 +0100

    Remove /illustrations

--- projects/urls.py
+++ projects/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^illustration/(?P<illustration_id>[0-9]+)/$', views.illustration, name='illustration'),
-    url(r'^illustrations/$', views.illustrations, name='illustrations'),
+    # url(r'^illustrations/$', views.illustrations, name='illustrations'),
     url(r'^illustration/(?P<illustration_id>[0-9]+)/edit', views.illustrationEdit, name='illustration-edit'),
 ]
