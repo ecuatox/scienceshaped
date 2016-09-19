d4702413308bceb9e1059c05bcfddaed9897ff4a
ecuatox <a.tenstad@gmail.com>
2016-09-19 09:34:36 +0200

    Fix cascade delete

--- projects/models.py
+++ projects/models.py
@@ -9,16 +9,16 @@ class Illustration(models.Model):
     description = models.TextField(max_length=500, blank=True, verbose_name='Description')
     tags = models.CharField(max_length=500, blank=True, verbose_name='Tags')
 
-    thumbnail = models.ForeignKey(Image, blank=True, null=True, related_name='%(app_label)s_%(class)s_related_thumb')
+    thumbnail = models.ForeignKey(Image, on_delete=models.SET_NULL, blank=True, null=True, related_name='%(app_label)s_%(class)s_related_thumb')
     thumbnail_size = models.CharField(max_length=4, default=100, verbose_name='Thumbnail size (%)')
 
     date = models.DateTimeField(default=timezone.now, verbose_name='Date')
 
     numberOfImages = models.IntegerField(verbose_name="Images")
 
-    image1 = models.ForeignKey(Image, blank=True, null=True, related_name='%(app_label)s_%(class)s_related_img1', verbose_name='Image 1')
-    image2 = models.ForeignKey(Image, blank=True, null=True, related_name='%(app_label)s_%(class)s_related_img2', verbose_name='Image 2')
-    image3 = models.ForeignKey(Image, blank=True, null=True, related_name='%(app_label)s_%(class)s_related_img3', verbose_name='Image 3')
+    image1 = models.ForeignKey(Image, on_delete=models.SET_NULL, blank=True, null=True, related_name='%(app_label)s_%(class)s_related_img1', verbose_name='Image 1')
+    image2 = models.ForeignKey(Image, on_delete=models.SET_NULL, blank=True, null=True, related_name='%(app_label)s_%(class)s_related_img2', verbose_name='Image 2')
+    image3 = models.ForeignKey(Image, on_delete=models.SET_NULL, blank=True, null=True, related_name='%(app_label)s_%(class)s_related_img3', verbose_name='Image 3')
 
     def __str__(self):
         return self.title
@@ -33,7 +33,7 @@ class Testimonial(models.Model):
     person = models.CharField(max_length=100, verbose_name='Person')
     job = models.CharField(max_length=300, verbose_name='Job')
     message = models.CharField(max_length=1000, verbose_name='Message')
-    thumbnail = models.ForeignKey(Image, blank=True, null=True)
+    thumbnail = models.ForeignKey(Image, on_delete=models.SET_NULL, blank=True, null=True)
     date = models.DateTimeField(default=timezone.now, verbose_name='Date')
 
     def __str__(self):
