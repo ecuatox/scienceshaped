d791c4654a9d048bd7e8ce9f41140188c5667dec
ecuatox <a.tenstad@gmail.com>
2017-03-04 19:54:22 +0100

    Run script at load

--- authentication/static/authentication/js/login.js
+++ authentication/static/authentication/js/login.js
@@ -1,16 +1,21 @@
-$('.submit').click(function(){
-  $('form').submit();
-});
+$(function() {
 
-$("#id_username").keypress(function(event) {
-  if (event.which == 13) {
-    event.preventDefault();
-    $("#id_password").focus();
-  }
-});
-$("#id_password").keypress(function(event) {
-  if (event.which == 13) {
-    event.preventDefault();
+  $('.submit').click(function(){
     $('form').submit();
-  }
+  });
+
+  $("#id_username").keypress(function(event) {
+    if (event.which == 13) {
+      event.preventDefault();
+      $("#id_password").focus();
+    }
+  });
+  
+  $("#id_password").keypress(function(event) {
+    if (event.which == 13) {
+      event.preventDefault();
+      $('form').submit();
+    }
+  });
+
 });
