57f211883e96741864d27932f2ad14dc9450ace9
ecuatox <a.tenstad@gmail.com>
2017-03-05 00:25:42 +0100

    Remove file from edit form

--- files/forms.py
+++ files/forms.py
@@ -10,7 +10,6 @@ class ImageEdit(forms.Form):
     title = forms.CharField(max_length=100, required=False)
     tags = forms.CharField(max_length=100, required=False)
     description = forms.CharField(widget=forms.Textarea, max_length=100, required=False)
-    file = forms.FileField(required=False)
 
 class ImageSearch(forms.Form):
     search = forms.CharField(max_length=100)
