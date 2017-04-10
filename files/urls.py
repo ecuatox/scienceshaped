2ffc23b482ec2f4d6021adfb6ae20c28c764fcf3
ecuatox <a.tenstad@gmail.com>
2017-04-10 15:26:39 +0200

    Fix urls

--- files/urls.py
+++ files/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     #url(r'^image/(?P<title>[0-9]+)/$', views.image, name='image'),
     url(r'^images/$', views.images, name='images'),
     url(r'^image-upload/$', views.imageUpload, name='image-upload'),
-    url(r'^image/(?P<image_id>[0-9]+)/delete', views.imageDelete, name='image-delete'),
-    url(r'^image/(?P<image_id>[0-9]+)/edit', views.imageEdit, name='image-edit'),
-    url(r'^image/(?P<image_id>[0-9]+)/view', views.imageView, name='image-view'),
+    url(r'^image/(?P<image_id>[0-9]+)/delete/$', views.imageDelete, name='image-delete'),
+    url(r'^image/(?P<image_id>[0-9]+)/edit/$', views.imageEdit, name='image-edit'),
+    url(r'^image/(?P<image_id>[0-9]+)/view/$', views.imageView, name='image-view'),
 ]
