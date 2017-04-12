6d5d51e443122ed9e660c73da073b2e7c88aa56f
ecuatox <a.tenstad@gmail.com>
2017-04-12 09:04:33 +0200

    Cleanup

--- scienceshaped/urls.py
+++ scienceshaped/urls.py
@@ -2,7 +2,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls import include, url
 from django.contrib import admin
-import authentication
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
